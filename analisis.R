1a98152fbd9ada058cfff3dc579c5f7e8fded613
urielmateos <Mateo@MacBook-Air-de-Prueba.local>
2025-08-11 16:03:18 -0600

    Añadir script para análisis de datos

---
+++ analisis.R
@@ -0,0 +1,6 @@
+# Este es un nuevo archivo para el repositorio
+set.seed(123)
+datos <- rnorm(100, mean = 50, sd = 10)
+
+# Creamos un histograma
+hist(datos, main = "Histograma de Datos", xlab = "Valores", ylab = "Frecuencia")
